2c61aa5ba14addff89d423f549c5f7df905e5c2f
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-26 00:59:01 +0600

    profile

--- index.js
+++ index.js
@@ -213,8 +213,16 @@ async function run() {
       res.send(result);
     })
 
-//Profile page
+//Profile page/////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// users profile API
+// app.post('/profile', async (req, res) => {
+
+//   const profile = req.body;
+//   const result = await profileCollection.insertOne(profile);
+//   res.send(result)
+// })
 app.post('/profile', async (req, res) => {
   const orders = req.body;
   console.log(orders.email);
@@ -226,6 +234,44 @@ app.post('/profile', async (req, res) => {
   const result = await profileCollection.insertOne(orders);
   res.send(result)
 })
+
+app.get('/profile', async (req, res) => {
+  const query = {};
+  const cursor = await profileCollection.find(query).toArray()
+
+  res.send(cursor);
+})
+
+
+app.get('/profile', async (req, res) => {
+
+  const email = req.query.email
+  const query = { email: email }
+  const cursor = profileCollection.find(query)
+  const profile = await cursor.toArray()
+  res.send(profile)
+})
+
+
+app.put('/profile/:id', async (req, res) => {
+  const id = req.params.id;
+  const updateProfileData = req.body
+  const filter = { _id: ObjectId(id) }
+  const options = { upsert: true }
+  const updateDoc = {
+      $set: {
+
+          education: updateProfileData.education,
+          location: updateProfileData.location,
+          linkdin: updateProfileData.linkdin,
+      }
+  };
+
+  const result = await profileCollection.updateOne(filter, updateDoc, options);
+  res.send(result)
+})
+
+
 
 
 // Payment Api
