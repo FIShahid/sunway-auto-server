983d523643a2a465e256abb4a567f2f164790f0d
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-25 14:25:52 +0600

    admin hook

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ async function run() {
     });
 
     /////Admin Api
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
 
 
     app.put('/user/admin/:email', verifyJWT, async (req, res) => {
